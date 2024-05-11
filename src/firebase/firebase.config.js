daac90de7715ec5716294d121b6fb93e01dd0408
zakir-naik <zakirhasanabir@gmail.com>
2024-05-11 15:08:44 +0600

    firebase setup and create error page

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";import { getAuth } from "firebase/a
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB251ixNXw-zRJPeugNQECgmgtmK5sXb6I",
-  authDomain: "food-donat-web.firebaseapp.com",
-  projectId: "food-donat-web",
-  storageBucket: "food-donat-web.appspot.com",
-  messagingSenderId: "788014564754",
-  appId: "1:788014564754:web:1ae4e58e4d5b8c759a2d4b"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import ErrorElement from "../pages/ErrorElement";
 
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
-    // errorElement: <ErrorElement></ErrorElement>,
+    errorElement: <ErrorElement></ErrorElement>,
     // children:[
     //   {
     //     path: '/',
